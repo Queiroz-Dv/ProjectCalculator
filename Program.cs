dda4cedd53221d74fcced703af8d306273ea9c38
Queiroz <kalifor_dbel@hotmail.com>
2022-03-18 16:21:14 -0300

    Implementação final

--- Program.cs
+++ Program.cs
@@ -1,4 +1,50 @@
 ﻿Console.Clear();
+Menu();
+
+static void Menu()
+{
+    const short SOMA = 1;
+    const short SUBTRACAO = 2;
+    const short DIVISAO = 3;
+    const short MULTIPLICACAO = 4;
+    const short SAIR = 5;
+
+    Console.Clear();
+    Console.WriteLine("Bem Vindo a Calculadora DV!");
+    Console.ReadKey();
+    Console.WriteLine("--------------------");
+    Console.WriteLine("Escolha uma opção!");
+    Console.WriteLine("1 - Soma");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Divisão");
+    Console.WriteLine("4 - Multiplicação");
+    Console.WriteLine("5 - Sair");
+    Console.WriteLine("--------------------");
+    Console.WriteLine("Selecione uma opção: ");
+
+    short res = short.Parse(Console.ReadLine());
+    switch (res)
+    {
+        case SOMA:
+            Soma();
+            break;
+        case SUBTRACAO:
+            Subtracao();
+            break;
+        case DIVISAO:
+            Divisao();
+            break;
+        case MULTIPLICACAO:
+            Multiplicacao();
+            break;
+        case SAIR:
+            Environment.Exit(0);
+            break;
+        default:
+            Menu();
+            break;
+    }
+}
 
 static void Soma()
 {
@@ -15,6 +61,7 @@ static void Soma()
     float soma = v1 + v2;
     Console.WriteLine("O resultado é: " + soma);
     Console.ReadKey();
+    Menu();
 }
 
 static void Subtracao()
@@ -33,6 +80,7 @@ static void Subtracao()
 
     Console.WriteLine("O resultado é: " + (v1 - v2));
     Console.ReadKey();
+    Menu();
 }
 
 static void Divisao()
@@ -51,6 +99,7 @@ static void Divisao()
 
     Console.WriteLine("O resultado é: " + (v1 / v2));
     Console.ReadKey();
+    Menu();
 }
 
 static void Multiplicacao()
@@ -67,4 +116,5 @@ static void Multiplicacao()
 
     Console.WriteLine("O resultado é:  " + (v1 * v2));
     Console.ReadKey();
+    Menu();
 }
