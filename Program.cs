d360b855d79180d130699350f2aad8009fb6040c
Queiroz <kalifor_dbel@hotmail.com>
2022-03-18 15:16:30 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+Console.Clear();
+Console.WriteLine("Digite um número: ");
+float v1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro número: ");
+float v2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine(""); //Pra pular linha
+
+float soma = v1 + v2;
+Console.WriteLine("O resultado é: " + soma);
