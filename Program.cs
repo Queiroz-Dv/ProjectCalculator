bd88023cfa4b57e60f5dd4028878882c8d1b8735
Queiroz <kalifor_dbel@hotmail.com>
2022-03-18 15:40:07 -0300

    Implementando métodos dos operadores

--- Program.cs
+++ Program.cs
@@ -1,13 +1,70 @@
-﻿using System.Xml.Serialization;
+﻿Console.Clear();
 
-Console.Clear();
-Console.WriteLine("Digite um número: ");
-float v1 = float.Parse(Console.ReadLine());
+static void Soma()
+{
+    Console.WriteLine("Bem-vindo ao método de Soma");
+    Console.ReadKey();
+    Console.WriteLine("Digite um número: ");
+    float v1 = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Digite outro número: ");
-float v2 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Digite outro número: ");
+    float v2 = float.Parse(Console.ReadLine());
 
-Console.WriteLine(""); //Pra pular linha
+    Console.WriteLine(""); //Pra pular linha
 
-float soma = v1 + v2;
-Console.WriteLine("O resultado é: " + soma);
+    float soma = v1 + v2;
+    Console.WriteLine("O resultado é: " + soma);
+    Console.ReadKey();
+}
+
+static void Subtracao()
+{
+    Console.Clear();
+    Console.WriteLine("Bem vindo ao Método de Subtração");
+    Console.ReadKey();
+
+    Console.WriteLine("Digite o primeiro número: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    Console.WriteLine("O resultado é: " + (v1 - v2));
+    Console.ReadKey();
+}
+
+static void Divisao()
+{
+    Console.Clear();
+    Console.WriteLine("Bem vindo ao Método de Divisão");
+    Console.ReadKey();
+
+    Console.WriteLine("Digite o primeiro número: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    Console.WriteLine("O resultado é: " + (v1 / v2));
+    Console.ReadKey();
+}
+
+static void Multiplicacao()
+{
+    Console.Clear();
+
+    Console.WriteLine("Bem Vindo ao Método de Multiplicação");
+    Console.ReadKey();
+    Console.WriteLine("Digite o primeiro número: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("O resultado é:  " + (v1 * v2));
+    Console.ReadKey();
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Calculator CSharp
+
+Projeto de estudos sobre a linguagem CSharp. Neste projeto realizei os estudos com base nos resumos realizados neste repositório(), a aplicação resume-se no desenvolvimento de uma calculadora.
